b4c2117f661c5bf9bce5d869889a91e36dbaf301
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-20 07:29:41 +0300

    added deployed url backend to the frontend

--- client/src/Auth/axiosInstance.js
+++ client/src/Auth/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const api = import.meta.env.VITE_API_URL
+// const api = import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
     // http://localhost:5000
-    baseURL:`${api}`,
+    baseURL:`https://fullstack-auth-o3vq.onrender.com`,
     withCredentials:true
 })
 
